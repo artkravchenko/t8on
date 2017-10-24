3b0382f133673855988171860cbca2eed81828b4
Artem Kravchenko <kravchenkofirst@gmail.com>
2017-10-24 19:33:41 +0700

    Fix unreachable "this" binding of deprecation wrapper function

--- src/util/deprecate.js
+++ src/util/deprecate.js
@@ -1,12 +1,11 @@
-module.exports = (f, message) => {
-  const g = function () {
-    if (!this.calledBefore) {
-      this.calledBefore = true;
+module.exports = function deprecate(f, message) {
+  let called = false;
+  return function deprecated() {
+    if (!called) {
+      called = true;
       // eslint-disable-next-line no-console
       console.warn(message);
     }
     return f.apply(null, arguments);
   };
-
-  return g;
 };
